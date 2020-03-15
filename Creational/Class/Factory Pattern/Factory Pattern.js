ecc054649a1f71ecfdf324949768e59248243321
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-03-15 12:14:41 +0100

    Factory Pattern Complete

--- Creational/Class/Factory Pattern/Factory Pattern.js
+++ Creational/Class/Factory Pattern/Factory Pattern.js
@@ -1,3 +1,13 @@
+/*
+Allows All Object Creations In A Centralised Location(removing repeating "const new object" creations).
+
+Use Ideas
+Clean Way Of Creating Employees/Users And Inserting Them Into DataBase.
+Helps With Tracking aswell. 
+*/
+
+// IMPlEMENTATION 1 Traditional(In This Case DataBase Is A Simple Array)
+
 function Developer(name) {
   this.name = name;
   this.type = "Developer";
@@ -13,10 +23,8 @@ function EmployeeFactory() {
     switch (type) {
       case 1:
         return new Developer(name);
-        break;
       case 2:
         return new Tester(name);
-        break;
     }
   };
 }
@@ -25,6 +33,7 @@ function say() {
   console.log("Hi, I am " + this.name + " and I am a " + this.type);
 }
 
+// creating objects
 const employeeFactory = new EmployeeFactory();
 const employees = [];
 
@@ -37,3 +46,49 @@ employees.push(employeeFactory.create("Tim", 2));
 employees.forEach(emp => {
   say.call(emp);
 });
+
+// IMPLEMENTATION 2 Modern
+
+class BallFactory {
+  constructor() {
+    this.createBall = function(type) {
+      let ball;
+      if (type === "football" || type === "soccer") ball = new Football();
+      else if (type === "basketball") ball = new Basketball();
+      ball.roll = function() {
+        return `The ${this._type} is rolling.`;
+      };
+
+      return ball;
+    };
+  }
+}
+
+class Football {
+  constructor() {
+    this._type = "football";
+    this.kick = function() {
+      return "You kicked the football.";
+    };
+  }
+}
+
+class Basketball {
+  constructor() {
+    this._type = "basketball";
+    this.bounce = function() {
+      return "You bounced the basketball.";
+    };
+  }
+}
+
+// creating objects
+const factory = new BallFactory();
+
+const myFootball = factory.createBall("football");
+const myBasketball = factory.createBall("basketball");
+
+console.log(myFootball.roll()); // The football is rolling.
+console.log(myBasketball.roll()); // The basketball is rolling.
+console.log(myFootball.kick()); // You kicked the football.
+console.log(myBasketball.bounce()); // You bounced the basketball.
