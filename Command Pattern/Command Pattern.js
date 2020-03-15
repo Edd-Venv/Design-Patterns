946834a08d17c53ac06bcb4c253a33d36adbaad7
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-03-15 11:07:50 +0100

    Command Pattern Complete

---
+++ Command Pattern/Command Pattern.js
@@ -0,0 +1,95 @@
+/*
+Uses
+The Abilty to Undo saving Users To A DataBase.
+Pros
+The Ability to Do and Undo Commands and The Commands Beginning Separate From the Object its Working on
+*/
+class Calculator {
+  constructor() {
+    this.value = 0;
+    this.history = [];
+  }
+
+  executeCommand(command) {
+    this.value = command.execute(this.value);
+    this.history.push(command);
+  }
+
+  undo() {
+    const command = this.history.pop();
+    this.value = command.undo(this.value);
+  }
+}
+
+class AddCommand {
+  constructor(valueToAdd) {
+    this.valueToAdd = valueToAdd;
+  }
+
+  execute(currentValue) {
+    return currentValue + this.valueToAdd;
+  }
+
+  undo(currentValue) {
+    return currentValue - this.valueToAdd;
+  }
+}
+
+class SubtractCommand {
+  constructor(valueToSubtract) {
+    this.valueToSubtract = valueToSubtract;
+  }
+
+  execute(currentValue) {
+    return currentValue - this.valueToSubtract;
+  }
+
+  undo(currentValue) {
+    return currentValue + this.valueToSubtract;
+  }
+}
+
+class MultiplyCommand {
+  constructor(valueToMultiply) {
+    this.valueToMultiply = valueToMultiply;
+  }
+
+  execute(currentValue) {
+    return currentValue * this.valueToMultiply;
+  }
+
+  undo(currentValue) {
+    return currentValue / this.valueToMultiply;
+  }
+}
+
+class DivideCommand {
+  constructor(valueToDivide) {
+    this.valueToDivide = valueToDivide;
+  }
+
+  execute(currentValue) {
+    return currentValue / this.valueToDivide;
+  }
+
+  undo(currentValue) {
+    return currentValue * this.valueToDivide;
+  }
+}
+
+class AddThenMultiplyCommand {
+  constructor(valueToAdd, valueToMultiply) {
+    this.addCommand = new AddCommand(valueToAdd);
+    this.multiplyCommand = new MultiplyCommand(valueToMultiply);
+  }
+
+  execute(currentValue) {
+    const newValue = this.addCommand.execute(currentValue);
+    return this.multiplyCommand.execute(newValue);
+  }
+
+  undo(currentValue) {
+    const newValue = this.multiplyCommand.undo(currentValue);
+    return this.addCommand.undo(newValue);
+  }
+}
